482a1b68dd9d48f4af465a5bd4943a5b57cef5f0
MyMac <ben.marcantel@gmail.com>
2018-04-19 10:58:49 -0500

    changed table name

--- models/user_product.js
+++ models/user_product.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   var User_product = sequelize.define('User_product', {
     liked: DataTypes.BOOLEAN
-  }, {});
+  }, { timestamps: false, tableName: "like"});
   User_product.associate = function(models) {
       liked: DataTypes.BOOLEAN      
   };
