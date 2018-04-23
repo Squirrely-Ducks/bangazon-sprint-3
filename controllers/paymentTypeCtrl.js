3050708ebf590771b81283181b6e8101ddf3b9ba
MyMac <ben.marcantel@gmail.com>
2018-04-23 10:09:18 -0500

    adding get types

--- controllers/paymentTypeCtrl.js
+++ controllers/paymentTypeCtrl.js
@@ -1,24 +1,8 @@
 "use strict";
 
-// Given the user is authenticated
-// When the user clicks on a profile affordance in the navigation bar
-// And the user clicks on the Add Payment Option hyperlink in their profile
-// Then the user can fill out the payment option form
-// And click Submit to save payment option
-
-const getUserPaymentType = (req,res,next)=>{
-    let id = req.app.get('user').id
-    Payment_Types.findById(id)
-    .then((data)=>{
-        console.log(data);
-    }) .catch(err=>{
-        console.log('ERROR:',err);
-    })
-} 
 
 const addPaymentType = (req, res, next)=>{
 
-    console.log(req.body);
 let {Payment_Types} = req.app.get('models');
 let user = req.app.get("user");
 let {...newPaymentType}= req.body;
@@ -26,9 +10,7 @@ let {...newPaymentType}= req.body;
 if (user){
     newPaymentType.userId = user.id;
     Payment_Types.create(newPaymentType)
-    .then((data)=>{
-        return getUserPaymentType()
-    }).then(()=>{
+    .then(()=>{
         res.render("welcome")   
     })
     .catch(err=>{
