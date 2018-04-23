0b9427a5ff2a5cabc1072f1a6f2c4a2b998afa95
Craig Orcutt <craigaorcutt@gmail.com>
2018-04-23 14:23:28 -0500

    added editing capabilities to account information

--- controllers/account-viewCtrl.js
+++ controllers/account-viewCtrl.js
@@ -1,12 +1,30 @@
-
-'use-strict';
+"use-strict";
 
 module.exports.myAccountView = (req, res) => {
-  const user = req.app.get('user')
-  res.render('my-account', { user });
+  const user = req.app.get("user");
+  const { User } = req.app.get("models");
+  User.findOne({where:{id: user.id}})
+  .then((foundUser)=>{
+    let userData = foundUser.dataValues
+    res.render("my-account", { userData });
+  })
+
 };
 
-
 module.exports.editAccount = (req, res, next) => {
-  
-}
+  const userId = req.app.get("user").id;
+  const { User } = req.app.get("models");
+  User.find({ where: { id: userId } })
+    .then(foundUser => {
+      return foundUser.updateAttributes({
+        street: req.body.street,
+        city: req.body.city,
+        state: req.body.state,
+        zip: req.body.zip,
+        phone: req.body.phone
+      });
+    })
+    .then(() => {
+      res.redirect(`/account`);
+    });
+};
