8c8b46afae69ca8e12f5ae84ff86a06389a8a719
Craig Orcutt <craigaorcutt@gmail.com>
2018-04-24 13:49:17 -0500

    added some minor styling changes to appeal to the masses

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const flash = require('express-flash');
 
 require('dotenv').config();
 const port = process.env.PORT || 8080;
-
+app.use(express.static(__dirname + "/css"));
 // using require('./models') to get the models may create more than one connection to the database. To avoid that, the models variable must be somehow singleton-esque. This can be achieved by attaching the models module to the application:
 app.set('models', require('./models')); //pulls in models/index.js by default. Index exports all the models you define in the models folder. So cool.
 // And when you need to require a class of the model in a controller, use this inside a middleware function rather than a direct import:
