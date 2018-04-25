96ad1f4a140f6db6da966321edefba0c36b9029a
JakeBrose <mordecaistudios@gmail.com>
2018-04-25 14:23:50 -0500

    added view for thank you message

--- controllers/ordersCtrl.js
+++ controllers/ordersCtrl.js
@@ -31,3 +31,25 @@ module.exports.cancelOrder = (req, res) => {
       res.redirect("/account")
     })
 };
+
+module.exports.selectPaymentType = (req, res) => {
+  const userId = req.app.get("user").id;
+  const { Payment_Types } = req.app.get("models");
+  Payment_Types.findAll({
+    where:{userId}
+  })
+    .then((viewPaymentTypes) => {
+      res.render("select-payment", { viewPaymentTypes })
+    })
+};
+
+module.exports.addPaymentToOrder = (req, res) => {
+  const userId = req.app.get("user").id;
+  const { Orders } = req.app.get("models");
+  Orders.update({paymentTypeId: req.params.id},
+  {where: {userId, paymentTypeId: null}}
+  )
+    .then(() => {
+      res.redirect("/account")
+    })
+};
--- routes/ordersRoute.js
+++ routes/ordersRoute.js
@@ -2,9 +2,12 @@
 
 const { Router } = require("express");
 const router = Router();
-const { viewShoppingCart, cancelOrder } = require('../controllers/ordersCtrl');
+const { viewShoppingCart, cancelOrder, selectPaymentType, addPaymentToOrder } = require('../controllers/ordersCtrl');
 
 router.get('/cart', viewShoppingCart);
 router.post("/cart/delete", cancelOrder);
+router.get("/cart/selectPayment", selectPaymentType);
+router.post("/cart/selectPayment/:id", addPaymentToOrder);
+
 
 module.exports = router;
